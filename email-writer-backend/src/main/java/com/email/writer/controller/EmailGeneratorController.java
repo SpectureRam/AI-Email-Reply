b2aaf4f449f65a49434d85613106ca7819251377
SAIRAM N <ramphilo@gmail.com>
2025-01-16 13:48:17 +0530

    Initial Add Files

---
+++ email-writer-backend/src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -0,0 +1,23 @@
+package com.email.writer.controller;
+
+import com.email.writer.service.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class EmailGeneratorController {
+    private final EmailGeneratorService emailGeneratorService;
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ email-writer-backend/src/main/java/com/email/writer/controller/EmailRequest.java
@@ -0,0 +1,8 @@
+package com.email.writer.controller;
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+}
